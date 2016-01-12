819dd9d524f9ff866468a70417a6a22cd13c9c31
Nagarkar, Vikram (UK) <vikram.nagarkar@euromoneyplc.com>
2016-01-12 17:05:48 +0000

    Added Parameter for Repository

--- git-robot.js
+++ git-robot.js
@@ -3,13 +3,30 @@ console.log('## Welcome to Git Robot ##');
 var Git = require("nodegit");
 var path = require("path");
 
+var argv = require('minimist')(process.argv.slice(2));
+var repo = null;
+if (argv.r) {
+    console.dir(argv);
+    repo = argv.r;
+} else console.log('Usage: -r <repository>');
+
+
 // Open the repository directory.
-Git.Repository.open(path.resolve(__dirname, "../../Projects/SauceLab/.git"))
+//Git.Repository.open(path.resolve(__dirname, "../../Projects/SauceLab/.git"))
+if (repo)
+Git.Repository.open(path.resolve(__dirname, repo))
+
   // Open the master branch.
   .then(success = function (repo) {
-      console.log('Repository opened successfully');
-      return repo.getMasterCommit();
+      console.log('Repository ' + argv.r + ' opened successfully');
+      repo.getBranch('octopack').then(function (ref) {
+          console.log('ref is ' + ref)
+          return ref;
+      });
+
+     return repo.getMasterCommit();
   }, error = function (err) { console.log(err.message)})
+
   // Display information about commits on master.
   .then(function (firstCommitOnMaster) {
       // Create a new history event emitter.
@@ -20,6 +37,7 @@ Git.Repository.open(path.resolve(__dirname, "../../Projects/SauceLab/.git"))
 
       // Listen for commit events from the history.
       history.on("commit", function (commit) {
+
           // Disregard commits past 9.
           if (++count >= 9) {
               return;
@@ -27,15 +45,16 @@ Git.Repository.open(path.resolve(__dirname, "../../Projects/SauceLab/.git"))
 
           // Show the commit sha.
           //console.log("commit " + commit.sha());
+          console.log("commit data " + commit);
 
           // Store the author object.
-          var author = commit.author();
+          //var author = commit.author();
 
           // Display author information.
-          console.log("Author:\t" + author.name() + " <" + author.email() + ">");
+          //console.log("Author:\t" + author.name() + " <" + author.email() + ">");
 
           // Show the commit date.
-          console.log("Date:\t" + commit.date());
+          //console.log("Date:\t" + commit.date());
 
           // Give some space and show the message.
           console.log("\n    " + commit.message());
